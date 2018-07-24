58493c9f2399ca869f9394410c29839b6b45ce03
heavenzyc <zyc>
2018-07-24 17:21:39 +0800

    sparkHive

--- spark-demo/src/main/scala/SparkHive.scala
+++ spark-demo/src/main/scala/SparkHive.scala
@@ -1,14 +1,18 @@
+import java.io.File
+
 import org.apache.spark.sql.SparkSession
+import org.joda.time.DateTime
 
 object SparkHive {
   def main(args: Array[String]): Unit = {
-    /*val conf = new SparkConf();
-    conf.setAppName("SparkSQL2Hive for scala")
-    conf.setMaster("local")
-
-    val sc = new SparkContext(conf)
-    val hiveContext = new HiveContext(sc)*/
-    val spark = SparkSession.builder().appName("Spark Hive Example").config("spark.sql.warehouse.dir", "spark-warehouse").enableHiveSupport().getOrCreate()
-    spark.sql("SELECT * FROM sparktest.student").show()
+    val warehouseLocation = new File("spark-warehouse").getAbsolutePath
+    println("warehouseLocation=" + warehouseLocation)
+    val spark = SparkSession.builder().appName("Spark Hive Example").config("spark.sql.warehouse.dir", warehouseLocation).enableHiveSupport().getOrCreate()
+    var current = new DateTime();
+    var userInfo = spark.sql("SELECT user_id FROM dmall_medusa.user_info ").where(" dt = '20180717' and group_key = '3111'" ).toDF()
+    var joinDF = userInfo.join(spark.sql("SELECT user_id, phone, sex, age, education FROM ads_data.user_profile"), Seq("user_id"), "inner")
+    joinDF.groupBy("sex").count().show()
+    joinDF.groupBy("education").count().show()
+    println("计算时长==============================" + (new DateTime().getMillis - current.getMillis))
   }
 }
