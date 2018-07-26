9ffaceabdb469010e1c924c8b94244f7499a7e61
heavenzyc <zyc>
2018-07-26 18:37:49 +0800

    sparksql

---
+++ spark-demo/src/main/scala/SparkSqlCollect.scala
@@ -0,0 +1,33 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object SparkSqlCollect {
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setAppName("SparkSQLDemo")
+    sparkConf.setMaster("local")
+    val spark = SparkSession.builder().appName("SparkSQLDemo").config(sparkConf).getOrCreate()
+    System.setProperty("hadoop.home.dir", "D:\\winutils")
+    runJDBCDataSource(spark)
+    //    loadDataSourceFromeJson(spark)
+    //    loadDataSourceFromeParquet(spark)
+    //    runFromRDD(spark)
+    spark.stop()
+  }
+
+  private def runJDBCDataSource(spark: SparkSession): Unit = {
+    val userInfo = spark.read.format("jdbc")
+      .option("url", "jdbc:mysql://192.168.8.207:13306/dmall_medusa?user=wumart&password=!QAZxsw2")
+      .option("driver", "com.mysql.jdbc.Driver")
+      .option("dbtable", "user_info") //必须写表名
+      .load().select("user_id", "group_id")
+    val userBase = spark.read.format("jdbc")
+      .option("url", "jdbc:mysql://192.168.8.207:13306/dmall_medusa?user=wumart&password=!QAZxsw2")
+      .option("driver", "com.mysql.jdbc.Driver")
+      .option("dbtable", "user_base_info") //必须写表名
+      .load().select("id", "sex", "education")
+    val joinDF = userInfo.groupBy("user_id").agg(collect_set("group_id") as "groupIds").join(userBase, userInfo("user_id") === userBase("id"), "inner")
+//    joinDF.groupBy("badge_no", "gender").count().show()
+
+  }
+}
--- spark-demo/src/main/scala/UserProfile.scala
+++ spark-demo/src/main/scala/UserProfile.scala
@@ -1,9 +1,8 @@
 import java.io.File
 
-import org.apache.spark.SparkConf
+import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.joda.time.DateTime
-import org.apache.spark.sql.functions._
 
 object UserProfile {
   def main(args: Array[String]): Unit = {
@@ -12,8 +11,7 @@ object UserProfile {
     val spark = SparkSession.builder().appName("Spark Hive Example").config("spark.sql.warehouse.dir", warehouseLocation).enableHiveSupport().getOrCreate()
     var current = new DateTime();
     var userInfo = spark.sql("SELECT user_id, group_id FROM dmall_medusa.user_info ").where(" dt = '20180717' and group_key in ('3111', '3200','3217')" ).toDF().limit(10)
-    userInfo.groupBy("user_id").agg(collect_set("group_id")).show()
-    var joinDF = userInfo.join(spark.sql("SELECT user_id, phone, sex, age, education FROM ads_data.user_profile"), Seq("user_id"), "inner")
+    val joinDF = userInfo.groupBy("user_id").agg(collect_set("group_id")).join(spark.sql("SELECT user_id, phone, sex, age, education FROM ads_data.user_profile"), Seq("user_id"), "inner")
     joinDF.groupBy("sex").count().show()
     joinDF.groupBy("education").count().show()
     println("计算时长==============================" + (new DateTime().getMillis - current.getMillis))
