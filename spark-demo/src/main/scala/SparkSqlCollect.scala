839286c3892f8515da7d321e57c225605b85d0be
heavenzyc <zyc>
2018-07-27 18:41:22 +0800

    计算画像

--- spark-demo/src/main/scala/SparkSqlCollect.scala
+++ spark-demo/src/main/scala/SparkSqlCollect.scala
@@ -1,6 +1,7 @@
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Column, DataFrame, Row, SparkSession}
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.expressions._
 
 object SparkSqlCollect {
   def main(args: Array[String]): Unit = {
@@ -21,21 +22,27 @@ object SparkSqlCollect {
       .option("driver", "com.mysql.jdbc.Driver")
       .option("dbtable", "user_info") //必须写表名
       .load().select("user_id", "group_id")
-    val groupIds = List(1, 2, 3)
+    /*val groupIds = List(1, 2, 3)
     for (id <- groupIds) {
       userInfo.filter(" group_id="+id).show()
-    }
+    }*/
 
     val userBase = spark.read.format("jdbc")
       .option("url", "jdbc:mysql://192.168.8.207:13306/dmall_medusa?user=wumart&password=!QAZxsw2")
       .option("driver", "com.mysql.jdbc.Driver")
       .option("dbtable", "user_base_info") //必须写表名
-      .load().select("id", "sex", "education")
-    val joinDF = userInfo
+      .load().select("id", "sex", "education", "age")
+    /*val joinDF = userInfo
       .groupBy("user_id")
       .agg(collect_set("group_id") as "groupIds")
-      .join(userBase, userInfo("user_id") === userBase("id"), "inner")
-//    joinDF.groupBy("badge_no", "gender").count().show()
+      .join(userBase, userInfo("user_id") === userBase("id"), "inner")*/
+    val cnt = userBase.filter((row) => row.getAs[Int]("age") >= 10 && row.getAs[Int]("age") <20).count()
+//    userBase.withColumn("age_agg", ageAgg(userBase("age").get)).show()
+  }
 
+  private def ageAgg(col : Int): Int = {
+    if (col.>=(10) && col.<(20))
+      return 1
+    return 0
   }
 }
