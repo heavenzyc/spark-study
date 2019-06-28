2936f3c4479a413e3af797cf55ece9537005e97b
heavenzyc <zyc>
2019-06-28 21:04:12 +0800

    美杜莎分析

--- spark-demo/src/main/scala/Medusa.scala
+++ spark-demo/src/main/scala/Medusa.scala
@@ -16,14 +16,28 @@ object Medusa {
     var dept = spark.sql("select id, dep_name from dmall_erp.erp_dep_info")
     var sta = spark.sql("select id, sta_name from dmall_erp.erp_sta_info")
     var user = groupInfo.join(baseUser, groupInfo("operate_user_id") === baseUser("id"))
-    var all = user.join(dept, user("id") === dept("id")).join(sta, user("id") === sta("id"))
+    var all = user.join(dept, user("dep_id") === dept("id")).join(sta, user("jobs_id") === sta("id"))
     all.select("operate_user_name", "operate_user_id", "cnt", "dep_id", "dep_name", "jobs_id", "sta_name").show()
-    all.toDF().registerTempTable("groupUser")
+    all.createOrReplaceTempView("groupUser")
     spark.sql("select * from groupUser").show()
     spark.sql("insert into ana_result.medusa_group_user select operate_user_name, operate_user_id, cnt, dep_id, dep_name, jobs_id, sta_name from groupUser")
   }
 
   def activityUser(spark : SparkSession): Unit = {
-    var activityInfo = spark.sql("""SELECT operate_user_id, operate_user_name, count(1) cnt  from dmall_medusa.push_activity where execute_start_time>='2019-02-01 00:00:00' and business_type=1  GROUP BY operate_user_id, operate_user_name order by cnt desc""");
+    var activityInfo = spark.sql(
+      """SELECT operate_user_id, operate_user_name, count(1) cnt
+        |from dmall_medusa.push_activity
+        |where execute_start_time>='2019-02-01 00:00:00'
+        |and business_type=1
+        |GROUP BY operate_user_id, operate_user_name
+        |order by cnt desc""")
+    activityInfo.count()
+    var baseUser = spark.sql("select id, dep_id  from dmall_erp.erp_base_user where user_status=1")
+    var dept = spark.sql("select id, dep_name from dmall_erp.erp_dep_info")
+    var user = activityInfo.join(baseUser, activityInfo("operate_user_id") === baseUser("id"))
+    var all = user.join(dept, user("dep_id") === dept("id"))
+    all.createOrReplaceTempView("activityUser") // 创建临时视图
+    // spark.catalog.dropTempView("activityInfo") // 删除临时视图
+    spark.sql("insert into ana_result.medusa_activity_user select operate_user_id,operate_user_name, cnt, dep_id, dep_name from activityUser")
   }
 }
